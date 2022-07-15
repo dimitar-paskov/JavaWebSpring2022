b22dd4bd34eb7f1ab683161d5ea392b7409c7c8a
dimitar_paskov <dimitarp2002@abv.bg>
2022-07-15 13:54:58 +0300

    Lecture01

---
+++ SP2022L01WebClient/src/main/java/bg/softuni/webclient/Sp2022L01WebClientApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp2022L01WebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp2022L01WebClientApplication.class, args);
+	}
+
+}
---
+++ SP2022L01WebClient/src/test/java/bg/softuni/webclient/Sp2022L01WebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp2022L01WebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
