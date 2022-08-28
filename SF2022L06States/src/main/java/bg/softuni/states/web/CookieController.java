fef503f449080090cb693f310416fce7e1b284a0
dimitar_paskov <dimitarp2002@abv.bg>
2022-08-28 12:22:36 +0300

    SF 2022 Lecture06

---
+++ SF2022L06States/src/main/java/bg/softuni/states/web/CookieController.java
@@ -0,0 +1,36 @@
+/**
+ * @author dimitar
+ *
+ */
+package bg.softuni.states.web;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CookieController {
+	
+	private static final String LANG_COOKIE_NAME="lang";
+	
+	@GetMapping("/cookies")
+	public String cookies(Model model, @CookieValue(name=LANG_COOKIE_NAME, defaultValue = "en") String lang) {
+		model.addAttribute(LANG_COOKIE_NAME, lang);
+		return "cookies";
+	}
+	
+	@PostMapping("/cookies")
+	public String cookies(HttpServletResponse response, @RequestParam("language") String language) {
+		
+		Cookie cookie = new Cookie(LANG_COOKIE_NAME, language);
+		response.addCookie(cookie);
+		return "redirect:/cookies";
+	}
+
+}
